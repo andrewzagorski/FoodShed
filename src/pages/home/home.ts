817e79b1218220f3bf4d3c4d9d5d37eb81cfe94a
Alec Chan <me@alecchan.io>
2018-10-05 23:29:32 -0500

    Added some dependencies for MapBox and set up boilerplate map interface to get started

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 
 @Component({
   selector: 'page-home',
@@ -8,6 +9,22 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidEnter() {
+
+    /*Initializing Map*/
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWVjaGFuIiwiYSI6ImNqbXd4ZmYzYTA0eWcza3J0NzVsNnNkcWoifQ.PQuBcFIs9STCQ6uf8DrJNw';
+    var map = new mapboxgl.Map({
+      style: 'mapbox://styles/aechan/cjmwxodn95lir2rmoq60ydb3m',
+      center: [43.0766, 89.4125],
+      zoom: 16,
+      pitch: 0,
+      minZoom: 1, //restrict map zoom - buildings not visible beyond 13
+      maxZoom: 17,
+      container: 'map'
+    });
 
   }
 
