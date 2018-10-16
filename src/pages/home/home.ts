5dba02dcc3b925b91cd30f1446d38bc27e255c34
Kenny Mui <klmui@wisc.edu>
2018-10-16 17:42:46 -0500

    one pin added, needs loop

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -25,7 +25,28 @@ export class HomePage {
       maxZoom: 17,
       container: 'map'
     });
-
+    map.on("load", function () {
+      /* Image: An image is loaded and added to the map. */
+      map.loadImage("https://i.imgur.com/MK4NUzI.png", function(error, image) {
+          if (error) throw error;
+          map.addImage("custom-marker", image);
+          /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+          map.addLayer({
+            id: "markers",
+            type: "symbol",
+            /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+            source: {
+              type: "geojson",
+              data: {
+                type: "FeatureCollection",
+                features:[{"type":"Feature","geometry":{"type":"Point","coordinates":[-89.40696937253546,43.071498310397885]}}]}
+            },
+            layout: {
+              "icon-image": "custom-marker",
+            }
+          });
+        });
+    });
   }
 
 }
