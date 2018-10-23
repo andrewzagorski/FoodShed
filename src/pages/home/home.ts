55a1f5a8d6b7ef941d77cf6adfc0b50e442f1d73
Merle <xzhang2229@wisc.edu>
2018-10-23 14:24:54 -0500

    find a new way to display a marker which is clickable and can show message, and the we might could use the message later. Find a nicer marker icon, but failed to use it. Add some code in package.json to let me run our app on my android

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -25,28 +25,42 @@ export class HomePage {
       maxZoom: 17,
       container: 'map'
     });
-    map.on("load", function () {
-      /* Image: An image is loaded and added to the map. */
-      map.loadImage("https://i.imgur.com/MK4NUzI.png", function(error, image) {
-          if (error) throw error;
-          map.addImage("custom-marker", image);
-          /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
-          map.addLayer({
-            id: "markers",
-            type: "symbol",
-            /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
-            source: {
-              type: "geojson",
-              data: {
-                type: "FeatureCollection",
-                features:[{"type":"Feature","geometry":{"type":"Point","coordinates":[-89.40696937253546,43.071498310397885]}}]}
-            },
-            layout: {
-              "icon-image": "custom-marker",
-            }
-          });
-        });
-    });
+    // map.on("load", function () {
+    //   /* Image: An image is loaded and added to the map. */
+    //   map.loadImage("https://i.imgur.com/MK4NUzI.png", function(error, image) {
+    //       if (error) throw error;
+    //       map.addImage("custom-marker", image);
+    //       /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+    //       map.addLayer({
+    //         id: "markers",
+    //         type: "symbol",
+    //         /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+    //         source: {
+    //           type: "geojson",
+    //           data: {
+    //             type: "FeatureCollection",
+    //             features:[{"type":"Feature","geometry":{"type":"Point","coordinates":[-89.40696937253546,43.071498310397885]}}]}
+    //         },
+    //         layout: {
+    //           "icon-image": "custom-marker",
+    //         }
+    //       });
+    //     });
+    // });
+    // create the popup
+    var popup = new mapboxgl.Popup({ offset: 25 })
+    .setText('Some food.');
+
+    // create DOM element for the marker
+    var el = document.createElement('div');
+    el.id = 'marker';
+
+    // create the marker
+    new mapboxgl.Marker(el)
+    .setLngLat([-89.40696937253546,43.071498310397885])
+    .setPopup(popup) // sets a popup on this marker
+    .addTo(map);
+
   }
 
 }
