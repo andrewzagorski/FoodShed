bad66d3d46d95336ca0609d5a32a147c5d9a6f86
Alec Chan <me@alecchan.io>
2018-11-06 18:01:45 -0600

    Worked on google sheets api stuff

--- src/pages/home/MapData.ts
+++ src/pages/home/MapData.ts
@@ -1,4 +1,5 @@
-import GoogleSpreadsheet from 'google-spreadsheet';
+import gapi from 'gapi-client';
+
 const SPREADSHEETID = '12uplyLstGU8m5404Xj_q2RbyVBnuOcIbP1ZgZYvEA_o';
 const WORKBOOKINDEX = 1;
 export class MapData {
@@ -25,20 +26,54 @@ export class MapData {
     }
 
     public static async retrieveMapData(): Promise<Array<MapData>> {
-        return null;
+        await gapi.load('client:auth2', this.initGapi);
+        await this.accessSpreadsheet();
+        
+        const data = await this.accessSpreadsheet();
+        return data;
     }
 
-    private static async accessSpreadsheet() {
-        const doc = new GoogleSpreadsheet(SPREADSHEETID);
+    private static async initGapi() {
+        await gapi.client.init({
+            apiKey: 'AIzaSyBQnYkNcTjiWhTtB8E5MSO2LJ7z9qN9ckg',
+            discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+            clientId: '69668555707-7o6e8bkqf0blp4140ofk790f65q9fi9f.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/spreadsheets.readonly'
+        });
+    }
 
-        // get the column containing location names
-        // returns an array of cell data
-        const locNames = await doc.getCells(WORKBOOKINDEX, {
-           // min-row: 1,
+    private static async accessSpreadsheet(): Promise<Array<MapData>> {
+        await gapi.client.init({
+            apiKey: 'AIzaSyBQnYkNcTjiWhTtB8E5MSO2LJ7z9qN9ckg',
+            discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+            clientId: '69668555707-7o6e8bkqf0blp4140ofk790f65q9fi9f.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/spreadsheets.readonly'
+        })
             
+        const names = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: SPREADSHEETID,
+            range: 'Locations!A2:A5',
         });
 
-        const statuses = await doc.
+        const statuses = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: SPREADSHEETID,
+            range: 'Locations!B2:B5'
+        });
+
+        const coords = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: SPREADSHEETID,
+            range: 'Locations!C2:C5'
+        });
+
+        console.log(`GOT: ${names}, ${statuses}, ${coords}`);
+
+        let mapDatas: MapData[] = [];
+
+        for(let i = 0; i < names.length; i++) {
+            mapDatas.push(new MapData(JSON.parse(coords[i]), names[i], statuses[i]));
+        }
+
+        return mapDatas;
     }
 
 }
