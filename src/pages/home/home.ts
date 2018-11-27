88d4adfcb6ab986b541a72d3f1b91abb4655ab3b
Merle <xzhang2229@wisc.edu>
2018-11-26 18:56:03 -0600

    successfully get data from spreadsheet but still some problem with async

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
-import { MapData } from './MapData';
+import request from 'request';
+import { getHostElement } from '@angular/core/src/render3';
 
 @Component({
   selector: 'page-home',
@@ -26,104 +27,76 @@ export class HomePage {
       maxZoom: 17,
       container: 'map'
     });
-    // map.on("load", function () {
-    //   /* Image: An image is loaded and added to the map. */
-    //   map.loadImage("https://i.imgur.com/MK4NUzI.png", function(error, image) {
-    //       if (error) throw error;
-    //       map.addImage("custom-marker", image);
-    //       /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
-    //       map.addLayer({
-    //         id: "markers",
-    //         type: "symbol",
-    //         /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
-    //         source: {
-    //           type: "geojson",
-    //           data: {
-    //             type: "FeatureCollection",
-    //             features:[{"type":"Feature","geometry":{"type":"Point","coordinates":[-89.40696937253546,43.071498310397885]}}]}
-    //         },
-    //         layout: {
-    //           "icon-image": "custom-marker",
-    //         }
-    //       });
-    //     });
-    // });
-    
-    // create the popup
-    // var popup = new mapboxgl.Popup({ offset: 25 })
-    //   .setText('Some food.');
 
-    // // create DOM element for the marker
-    // var el = document.createElement('div');
-    // el.id = 'marker';
+    // geojson format example
+    // var geojson = {
+    //   type: 'FeatureCollection',
+    //   features: [{
+    //     type: 'Feature',
+    //     geometry: {
+    //       type: 'Point',
+    //       coordinates: [-89.4155519, 43.077424]
+    //     },
+    //     properties: {
+    //       title: 'Sullivan Residence Hall',
+    //       description: "Sullivan's fridge is empty"
+    //     }
+    //   },
+    //   {
+    //     type: 'Feature',
+    //     geometry: {
+    //       type: 'Point',
+    //       coordinates: [-89.41778898239136, 43.07775948865735]
+    //     },
+    //     properties: {
+    //       title: 'Dejope Residence Hall',
+    //       description: "Dejope's fridge is stocked"
+    //     }
+    //   },
+    //   {
+    //     type: 'Feature',
+    //     geometry: {
+    //       type: 'Point',
+    //       coordinates: [-89.40695285797119, 43.07677990128432]
+    //     },
+    //     properties: {
+    //       title: 'Elizabeth Waters Residence Hall',
+    //       description: "Waters' fridge is stocked"
+    //     }
+    //   },
+    //   {
+    //     type: 'Feature',
+    //     geometry: {
+    //       type: 'Point',
+    //       coordinates: [-89.4012451171875, 43.07382537092813]
+    //     },
+    //     properties: {
+    //       title: 'Chadbourne Residence Hall',
+    //       description: "Chad's fridge are stocked"
+    //     }
+    //   },
+    //   {
+    //     type: 'Feature',
+    //     geometry: {
+    //       type: 'Point',
+    //       coordinates: [-89.40125584602356, 43.076764227759085]
+    //     },
+    //     properties: {
+    //       title: 'College Liberary',
+    //       description: "CoLi's fridge are stocked"
+    //     }
+    //   }]
+    // };
 
-    // // create the marker
-    // new mapboxgl.Marker(el)
-    //   .setLngLat([-89.40696937253546, 43.071498310397885])
-    //   .setPopup(popup) // sets a popup on this marker
-    //   .addTo(map);
-    
 
-    // TODO: 1.try ro make geojson a external file; 2. use spreadsheet api to update data. 
-    // Get coordinates from https://www.gps-coordinates.net/. Maybe can add feature that user can add fridges themselves
-    var geojson = {
-      type: 'FeatureCollection',
-      features: [{
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-89.4155519, 43.077424]
-        },
-        properties: {
-          title: 'Sullivan Residence Hall',
-          description: "Sullivan's fridge is empty"
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-89.41778898239136, 43.07775948865735]
-        },
-        properties: {
-          title: 'Dejope Residence Hall',
-          description: "Dejope's fridge is stocked"
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-89.40695285797119, 43.07677990128432]
-        },
-        properties: {
-          title: 'Elizabeth Waters Residence Hall',
-          description: "Waters' fridge is stocked"
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-89.4012451171875, 43.07382537092813]
-        },
-        properties: {
-          title: 'Chadbourne Residence Hall',
-          description: "Chad's fridge are stocked"
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-89.40125584602356, 43.076764227759085]
-        },
-        properties: {
-          title: 'College Liberary',
-          description: "CoLi's fridge are stocked"
-        }
-      }]
-    };
+    var geojson;
+    // this function works correctly. It can get correct format geojson. But I dont get the asynchronous things
+
+    // TODO now it can get correct-formated geojson. But the following foreach will run before it download the data, and output "geojson undefined" error
+    var url = 'https://script.google.com/macros/s/AKfycbx9F0ORXrp-IiX0t7LmRxVyeKEk57Vu3wVrsbl_uzfkLGhfGh8/exec'
+    request.get(url, function (error, response, body) {
+      geojson = JSON.parse(body);
+    })  
 
     // add markers to map
     geojson.features.forEach(function (marker) {
@@ -136,10 +109,11 @@ export class HomePage {
       new mapboxgl.Marker(el)
         .setLngLat(marker.geometry.coordinates)
         .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-        .setHTML('<h3>' + marker.properties.title + '</h3><p><div align="center">' + marker.properties.description + '</div></p>'))
+          .setHTML('<h3>' + marker.properties.title + '</h3><p><div align="center">' + marker.properties.description + '</div></p>'))
         .addTo(map);
     });
 
   }
 
 }
+
