e2d8427fe27d67f1be25bb3d41ea7f7071313f2a
Merle <xzhang2229@wisc.edu>
2018-11-27 19:06:07 -0600

    finally work perfectly. Change request to request-promise, so it will return a promise that we can use. TODO: change url to official sheet and we are done!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
-import request from 'request';
-import { getHostElement } from '@angular/core/src/render3';
+import rp from 'request-promise';
 
 @Component({
   selector: 'page-home',
@@ -28,96 +27,88 @@ export class HomePage {
       container: 'map'
     });
 
-    // geojson format example
-    // var geojson = {
-    //   type: 'FeatureCollection',
-    //   features: [{
-    //     type: 'Feature',
-    //     geometry: {
-    //       type: 'Point',
-    //       coordinates: [-89.4155519, 43.077424]
-    //     },
-    //     properties: {
-    //       title: 'Sullivan Residence Hall',
-    //       description: "Sullivan's fridge is empty"
-    //     }
-    //   },
-    //   {
-    //     type: 'Feature',
-    //     geometry: {
-    //       type: 'Point',
-    //       coordinates: [-89.41778898239136, 43.07775948865735]
-    //     },
-    //     properties: {
-    //       title: 'Dejope Residence Hall',
-    //       description: "Dejope's fridge is stocked"
-    //     }
-    //   },
-    //   {
-    //     type: 'Feature',
-    //     geometry: {
-    //       type: 'Point',
-    //       coordinates: [-89.40695285797119, 43.07677990128432]
-    //     },
-    //     properties: {
-    //       title: 'Elizabeth Waters Residence Hall',
-    //       description: "Waters' fridge is stocked"
-    //     }
-    //   },
-    //   {
-    //     type: 'Feature',
-    //     geometry: {
-    //       type: 'Point',
-    //       coordinates: [-89.4012451171875, 43.07382537092813]
-    //     },
-    //     properties: {
-    //       title: 'Chadbourne Residence Hall',
-    //       description: "Chad's fridge are stocked"
-    //     }
-    //   },
-    //   {
-    //     type: 'Feature',
-    //     geometry: {
-    //       type: 'Point',
-    //       coordinates: [-89.40125584602356, 43.076764227759085]
-    //     },
-    //     properties: {
-    //       title: 'College Liberary',
-    //       description: "CoLi's fridge are stocked"
-    //     }
-    //   }]
-    // };
-
-
-    // this function works correctly. It can get correct format geojson. But I dont get the asynchronous things
-    
-    // TODO now it can get correct-formated geojson. But the following foreach will run before it download the data, and output "geojson undefined" error
-    
-    var geojson;
     var url = 'https://script.google.com/macros/s/AKfycbx9F0ORXrp-IiX0t7LmRxVyeKEk57Vu3wVrsbl_uzfkLGhfGh8/exec'
-    request.get(url, function (error, response, body) {
-      geojson = JSON.parse(body);
-    // })  //change it to line 117 
-  
-
+    rp.get(url, function (error, response, body) {
+      return body;
+    })
     // add markers to map
-    geojson.features.forEach(function (marker) {
+    .then((body) => {
+      let geojson = JSON.parse(body)
+      geojson.features.forEach(function (marker) {
 
-      // create a HTML element for each feature
-      var el = document.createElement('div');
-      el.id = 'marker';
+        // create a HTML element for each feature
+        var el = document.createElement('div');
+        el.id = 'marker';
 
-      // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el)
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
         .setLngLat(marker.geometry.coordinates)
         .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-          .setHTML('<h3>' + marker.properties.title + '</h3><p><div align="center">' + (marker.properties.description?"stocked":"empty") + '</div></p>'))
-        .addTo(map);
-    });
-    })
-    
-   
+        .setHTML('<h3>' + marker.properties.title + '</h3>'
+          + '<p><div align="center">' + (marker.properties.description?"stocked":"empty") + '</div></p>'))
+          .addTo(map);
+        });
+      })
+    }
   }
-
-}
-
+  
+  // geojson format example
+  // var geojson = {
+  //   type: 'FeatureCollection',
+  //   features: [{
+  //     type: 'Feature',
+  //     geometry: {
+  //       type: 'Point',
+  //       coordinates: [-89.4155519, 43.077424]
+  //     },
+  //     properties: {
+  //       title: 'Sullivan Residence Hall',
+  //       description: "Sullivan's fridge is empty"
+  //     }
+  //   },
+  //   {
+  //     type: 'Feature',
+  //     geometry: {
+  //       type: 'Point',
+  //       coordinates: [-89.41778898239136, 43.07775948865735]
+  //     },
+  //     properties: {
+  //       title: 'Dejope Residence Hall',
+  //       description: "Dejope's fridge is stocked"
+  //     }
+  //   },
+  //   {
+  //     type: 'Feature',
+  //     geometry: {
+  //       type: 'Point',
+  //       coordinates: [-89.40695285797119, 43.07677990128432]
+  //     },
+  //     properties: {
+  //       title: 'Elizabeth Waters Residence Hall',
+  //       description: "Waters' fridge is stocked"
+  //     }
+  //   },
+  //   {
+  //     type: 'Feature',
+  //     geometry: {
+  //       type: 'Point',
+  //       coordinates: [-89.4012451171875, 43.07382537092813]
+  //     },
+  //     properties: {
+  //       title: 'Chadbourne Residence Hall',
+  //       description: "Chad's fridge are stocked"
+  //     }
+  //   },
+  //   {
+  //     type: 'Feature',
+  //     geometry: {
+  //       type: 'Point',
+  //       coordinates: [-89.40125584602356, 43.076764227759085]
+  //     },
+  //     properties: {
+  //       title: 'College Liberary',
+  //       description: "CoLi's fridge are stocked"
+  //     }
+  //   }]
+  // };
+  
